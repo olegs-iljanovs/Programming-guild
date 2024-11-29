2793ca43e536b69bf2f3104665a24a2138ef9307
yaroslavkhoda <yaroslav.khoda@clarity.cx>
2024-11-29 16:05:41 +0200

    Data validation added

--- sales_report.py
+++ sales_report.py
@@ -1,28 +1,32 @@
 def sales_report(sales_data, top_n):
+    #data validation
+    if len(sales_data) < 1 or top_n < 1:
+        return "Put valid data"
+    for item in sales_data:
+        if not isinstance(item.get("name"), str):
+            return f"Missing name in {item}"
+        if not isinstance(item.get("sales"), float):
+            return f"Missing sales in {item}" 
+        
+
     top_performers = f'''Top {top_n} Sales Performers:\n'''
-    try:
-        sales_data.sort(reverse = True, key=lambda person: person.get("sales")) #sorting list based on sales
+    sales_data.sort(reverse = True, key=lambda person: person.get("sales", 0.00)) #sorting list based on sales
 
-        #filling output string
-        for n, _ in enumerate(sales_data):
-            if n >= top_n:
-                break
-            top_performers += f'{n+1}. {sales_data[n].get("name")} - ${sales_data[n].get("sales")}\n'
-            
-        return top_performers
-    except TypeError:
-        return("Sales field should be float")
-    except:
-        return("Something went wrong. Check data and try again")
-
+    #filling output string
+    for n, _ in enumerate(sales_data):
+        if n >= top_n:
+            break
+        top_performers += f'{n+1}. {sales_data[n].get("name", "EmptyName")} - ${sales_data[n].get("sales", 0.00)}\n'
+        
+    return top_performers
 
 #data for testing
 sales_data = [
-{"name": "Alice", "sales": 3500.00},
+{"name": "Alice", "sales": ""},
 {"name": "Bob", "sales": 2000.50},
 {"name": "Charlie", "sales": 5000.00},
 {"name": "David", "sales": 4200.00},
 {"name": "Eva", "sales": 3000.00}
  ]
-a = sales_report(sales_data, 3)
+a = sales_report(sales_data, 32)
 print(a)
