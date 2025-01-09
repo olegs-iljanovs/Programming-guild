ff32d8c327772e6df58a253714d272ea59b88f65
yaroslavkhoda <yaroslav.khoda@clarity.cx>
2025-01-09 11:54:13 +0200

    Update main.py

--- Library Management System Yaroslav/main.py
+++ Library Management System Yaroslav/main.py
@@ -1,23 +1,29 @@
 import sqlite3
 
-def add_book(cusros):
-    pass
+def add_book(cursor):
+    title = input("Please, insert title:\n")
+    author = input("And author:\n")
+    cursor.execute(f"INSERT INTO book (title, author, available) VALUES ('{title}', '{author}', True)")
 
-def borrow_book(cusros):
-    pass
+def borrow_book(cursor):
+    pk = input("Please, insert id:\n")
+    cursor.execute(f"UPDATE book SET available = False WHERE pk = '{pk}'")
 
-def return_book(cusros):
-    pass
+def return_book(cursor):
+    pk = input("Please, insert id:\n")
+    cursor.execute(f"UPDATE book SET available = True WHERE pk = '{pk}'")
 
-def view_available_books(cusros):
-    pass
+def view_available_books(cursor):
+    cursor.execute('''SELECT * FROM book WHERE available''')
+    output = cursor.fetchall()
+    return output
 
 if __name__ == "__main__":
 
     connection = sqlite3.connect("library.db")
     cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS book(title TEXT, author TEXT, available BINARY)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS book(pk INTEGER NOT NULL PRIMARY KEY, title TEXT, author TEXT, available BINARY)")
 
 
     while True:
@@ -29,7 +35,8 @@ if __name__ == "__main__":
         5) Exit\n
             '''))
         if menu == 1:
-            view_available_books(cursor)
+            for row in view_available_books(cursor):
+                print(row)
         elif menu == 2:
             borrow_book(cursor)
         elif menu == 3:
@@ -38,6 +45,7 @@ if __name__ == "__main__":
             add_book(cursor)
         else:
             print("See you later!")
+            connection.commit()
             break
         
     connection.close()
